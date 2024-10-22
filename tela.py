aa3eae70cc091732c972e738d37585f048999924
raposoxxx <juliarferreira00@gmail.com>
2024-10-22 14:05:19 -0300

    fix: pygame methods

--- tela.py
+++ tela.py
@@ -10,27 +10,26 @@ timer = pygame.time.Clock()
 
 class Tela():
 
-    def produzirTela(): # aparece tela
-        WIDTH = 500
-        HEIGHT = 700
-        screen = pygame.display.set_mode([WIDTH, HEIGHT])
+    def __init__(self):
+        self.WIDTH = 500
+        self.HEIGHT = 700
+        self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])  # A tela é definida aqui
         pygame.display.set_caption("Palavreado")
-
-        screen.fill(white)
-
-    def produzirMatriz(): # cria a matriz das palavras
-        global screen
-        turn = 0
-        board = [[" ", " ", " ", " ", " "],
-                [" ", " ", " ", " ", " "],
-                [" ", " ", " ", " ", " "],
-                [" ", " ", " ", " ", " "],
-                [" ", " ", " ", " ", " "],
-                [" ", " ", " ", " ", " "],]
+        self.board = [[" ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " "]]
         
+
+    def produzirTela(self): # aparece tela
+        self.screen.fill(white)
+
+    def produzirMatriz(self): # cria a matriz das palavras
         for col in range(0, 5):
             for row in range(0, 6):
-                pygame.draw.rect(screen, black, [col * 100, row * 100, 75, 75], 3, 5)
+                pygame.draw.rect(self.screen, black, [col * 100, row * 100, 75, 75], 3, 5)
 
 
 tela = Tela()
