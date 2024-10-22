efb75397f0f383a5d8250e82a3dd5f66e3588f65
raposoxxx <juliarferreira00@gmail.com>
2024-10-22 15:09:39 -0300

    feat: color change

--- tela.py
+++ tela.py
@@ -5,10 +5,12 @@ pygame.init()
 white = (255, 255, 255)
 gray = (48, 48, 48)
 green = (19, 122, 53)
+yellow = (220, 176, 53)
     
 fps = 60
 timer = pygame.time.Clock()
 titleFont = pygame.font.Font('freesansbold.ttf', 56)
+turnActive = True
 
 class Tela():
 
@@ -24,6 +26,9 @@ class Tela():
                       [" ", " ", " ", " ", " "],
                       [" ", " ", " ", " ", " "]]
         
+        self.turn = 0
+        self.letters = 0
+        
 
     def produzirTela(self): # aparece tela
         self.screen.fill(white)
@@ -33,20 +38,50 @@ class Tela():
             for row in range(0, 6):
                 pygame.draw.rect(self.screen, gray, [col * 100 + 12, row * 100 + 12, 75, 75], 3, 5)
                 pieceText = titleFont.render(self.board[row][col], True, gray)
-                self.screen.blit(pieceText, (col * 100 + 35, row * 100 + 25))
+                self.screen.blit(pieceText, (col * 100 + 30, row * 100 + 25))
+
+    def checarPalavra(self):
+        global palavra
+        for col in range(0, 5):
+            for row in range(0, 6):
+                if palavra[col] == self.board[row][col] and self.turn > row:
+                    pygame.draw.rect(self.screen, green, [col * 100 + 12, row * 100 + 12, 75, 75], 0, 5)
+                elif self.board[row][col] in palavra and self.turn > row:
+                    pygame.draw.rect(self.screen, yellow, [col * 100 + 12, row * 100 + 12, 75, 75], 0, 5)
+
 
 
 tela = Tela()
+
 
 running = True
 while running:
     timer.tick(fps)
     tela.produzirTela()
     tela.produzirMatriz()
+    tela.checarPalavra()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE and tela.letters > 0:
+                tela.board[tela.turn][tela.letters] = ' '
+                tela.letters -= 1
+            if event.key == pygame.K_SPACE:
+                tela.turn += 1
+                tela.letters = 0
+        if event.type == pygame.TEXTINPUT:
+            entry = event.__getattribute__('text')
+            tela.board[tela.turn][tela.letters] = entry.upper()
+            tela.letters += 1
+        
+
+    if tela.letters == 5:
+        turnActive = False
+    if tela.letters < 5:
+        turnActive = True
+
 
     pygame.display.flip()
 
