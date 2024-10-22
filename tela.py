1dcea367bfd9bd1e922376ec46168eca55aa8497
raposoxxx <juliarferreira00@gmail.com>
2024-10-22 16:36:47 -0300

    feat: file management

--- tela.py
+++ tela.py
@@ -1,6 +1,9 @@
 import pygame
+from jogo import Jogo
 
 pygame.init()
+
+jogo = Jogo()
 
 white = (255, 255, 255)
 gray = (48, 48, 48)
@@ -11,6 +14,7 @@ fps = 60
 timer = pygame.time.Clock()
 titleFont = pygame.font.Font('freesansbold.ttf', 56)
 turnActive = True
+
 
 class Tela():
 
@@ -28,7 +32,7 @@ class Tela():
         
         self.turn = 0
         self.letters = 0
-        
+        self.palavra = jogo.escolherPalavra().upper()
 
     def produzirTela(self): # aparece tela
         self.screen.fill(white)
@@ -37,17 +41,23 @@ class Tela():
         for col in range(0, 5):
             for row in range(0, 6):
                 pygame.draw.rect(self.screen, gray, [col * 100 + 12, row * 100 + 12, 75, 75], 3, 5)
-                pieceText = titleFont.render(self.board[row][col], True, gray)
-                self.screen.blit(pieceText, (col * 100 + 30, row * 100 + 25))
 
-    def checarPalavra(self):
-        global palavra
+
+    def preencherMatriz(self):
         for col in range(0, 5):
             for row in range(0, 6):
-                if palavra[col] == self.board[row][col] and self.turn > row:
+                pieceText = titleFont.render(self.board[row][col], True, gray)
+                self.screen.blit(pieceText, (col * 100 + 30, row * 100 + 25))        
+
+    def checarPalavra(self):
+        for col in range(0, 5):
+            for row in range(0, 6):
+                if self.palavra[col] == self.board[row][col]:
                     pygame.draw.rect(self.screen, green, [col * 100 + 12, row * 100 + 12, 75, 75], 0, 5)
-                elif self.board[row][col] in palavra and self.turn > row:
+                elif self.board[row][col] in self.palavra:
                     pygame.draw.rect(self.screen, yellow, [col * 100 + 12, row * 100 + 12, 75, 75], 0, 5)
+            
+
 
 
 
@@ -56,31 +66,36 @@ tela = Tela()
 
 running = True
 while running:
+    
+    if tela.letters == 5:
+        turnActive = False
+    if tela.letters < 5:
+        turnActive = True
+
     timer.tick(fps)
     tela.produzirTela()
     tela.produzirMatriz()
-    tela.checarPalavra()
+    tela.preencherMatriz()
+    tela.checarPalavra() 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE and tela.letters > 0:
-                tela.board[tela.turn][tela.letters] = ' '
+                tela.board[tela.turn][tela.letters - 1] = ' '
                 tela.letters -= 1
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_TAB:
                 tela.turn += 1
                 tela.letters = 0
-        if event.type == pygame.TEXTINPUT:
+                turnActive = True
+        if event.type == pygame.TEXTINPUT and turnActive:
             entry = event.__getattribute__('text')
             tela.board[tela.turn][tela.letters] = entry.upper()
             tela.letters += 1
         
 
-    if tela.letters == 5:
-        turnActive = False
-    if tela.letters < 5:
-        turnActive = True
+
 
 
     pygame.display.flip()
