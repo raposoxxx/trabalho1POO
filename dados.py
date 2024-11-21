7acd25953b8ffca9f0322123cf3e623fc7e55665
gabrielrolla <gabrielrolla@me.com>
2024-11-21 16:40:19 -0300

    a

--- dados.py
+++ dados.py
@@ -17,11 +17,10 @@ def csvToJson(csv_filename, json_filename) -> None:
     df.to_json(json_filename, orient='records', lines=True)
 
 
-def readJson(filename) -> None:
+def readJson(filename) -> list:
     # Lê o JSON e retorna a coluna de palavras como lista
     df = pd.read_json(filename, orient='records', lines=True)
     return df[0].tolist()
-
 
 def atualizarVitorias(json_filename, nome_jogador):
     try:
